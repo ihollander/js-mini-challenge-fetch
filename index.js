eddd3e8846284508952a4ddcd55928fbab9c38ae
billyott <willrichott@gmail.com>
2020-10-29 22:00:09 -0400

    refactor + cleanup

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-//variables
+//variables for targeting html
 const playerContainer = document.querySelector(".player-container")
-const header = document.querySelector("h1#header")
 const newPlayerForm = document.querySelector("#new-player-form")
 
-//functions + handlers
+//functions for handling events and such
 function renderPlayer(player) {
     const playerDiv = document.createElement("div")
     playerDiv.className = "player"
-    playerDiv.id = player.id
+    playerDiv.dataset.id = player.id
     playerDiv.dataset.number = player.number
     playerDiv.innerHTML = `
     <h3>${player.name} (<em>${player.nickname}</em>)</h3>
@@ -22,38 +21,23 @@ function renderEachPlayer(array) {
     array.forEach(renderPlayer)
 }
 
-fetch("http://localhost:3000/players")
-.then(response => response.json())
-.then(data => renderEachPlayer(data))    
-
-
-function toggleColor(element) {
-    if (element.style.color === "red") {
-        element.style.color = "black"
-    } else {
-        element.style.color = "red"
-    }
-}
-
-function handleToggleColor(e) {
-    toggleColor(e.target)
+function renderCurrentPlayers() {
+    fetch("http://localhost:3000/players")
+    .then(response => response.json())
+    .then(data => renderEachPlayer(data))
 }
 
 //event listeners
 newPlayerForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    const number = e.target.number.value
-    const name = e.target.name.value
-    const nickname = e.target.nickname.value
-    const photo = e.target.photo.value
     
     const playerObj = {
-        number: number,
-        name: name,
-        nickname: nickname,
-        photo: photo,
-        likes: 1000
-    }
+        number: e.target.number.value,
+        name: e.target.name.value,
+        nickname: e.target.nickname.value,
+        photo: e.target.photo.value,
+        likes: 999
+      }
 
     const configObj = {
         method: "POST",
@@ -73,16 +57,13 @@ newPlayerForm.addEventListener('submit', function (e) {
     e.target.reset()
 })
 
-header.addEventListener('click', handleToggleColor)
-
 playerContainer.addEventListener('click', function (e) {
     if (e.target.matches(".like-button")) {
         const likeButton = e.target
         const playerDiv = likeButton.closest(".player")
         const likesPtag = playerDiv.querySelector(".likes")
-        const likesCount = parseInt(likesPtag.textContent.replace(' likes',''))
+        const likesCount = parseInt(likesPtag.textContent)
         likesPtag.textContent = likesCount + 1 + ' likes'
-        const playerId = playerDiv.id
         const updatedLikes = { likes: parseInt(likesPtag.textContent) }
 
         const configObj = {
@@ -93,10 +74,11 @@ playerContainer.addEventListener('click', function (e) {
             body: JSON.stringify(updatedLikes)
         };
         
-        fetch(`http://localhost:3000/players/${playerId}`, configObj)
+        fetch(`http://localhost:3000/players/${playerDiv.dataset.id}`, configObj)
         .then(response => response.json())
         .then(data => console.log("Success", data))
     }
 })
 
-
+//invoke initial render upon page load
+renderCurrentPlayers()
